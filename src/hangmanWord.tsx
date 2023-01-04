5ce66bd1497d26bcf815a7cb8b59547dc3fc9ba4
AnujChhikara <anujchhikara777@gmail.com>
2023-01-04 21:03:24 +0530

    day2

--- src/hangmanWord.tsx
+++ src/hangmanWord.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 function HangmanWord() {
+  const word = "TEST"
+  const guessedLetters = ["T"]
   return (
-    <div>
-      
+    <div className='flex gap-1 font-mono font-semibold text-4xl '>
+     {word.split("").map((letter, index) => (
+      <span className=' border-black  border-b-4'><span className={`${guessedLetters.includes(letter) ? "visible" : "hidden"}  `}>{letter}</span></span>
+     ))}
     </div>
   )
 }
